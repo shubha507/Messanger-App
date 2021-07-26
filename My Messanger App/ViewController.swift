9b381c9f296c8de2a4a17357241c230f5f2d7982
Shubha Sachan <shubha.sachan@tatadigital.com>
2021-07-26 11:41:15 +0530

    Initial Commit

---
+++ My Messanger App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Messanger App
+//
+//  Created by Shubha Sachan  on 26/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
